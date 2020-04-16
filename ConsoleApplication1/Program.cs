78da27d7dacd7982dc20204ab9cefdcdd31777cd
maximilianoescribano <maximilianoescribano@gmail.com>
2020-04-16 13:55:59 -0300

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar cantidad de Instancias and press Enter");
+            var num = Convert.ToUInt16(Console.ReadLine());
+            for (var i = 0; i < num; i++)
+            {
+                var opt = new ChromeOptions();
+                opt.AddArgument("--ignore-certificate-errors");
+                var driver = new ChromeDriver(opt);
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://stream.clawstudios.com:8443");
+                driver.FindElementByCssSelector("#viewer").Click();
+            }
+        }
+    }
+}
